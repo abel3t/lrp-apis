76bad37b1f07ca450f8e2abd2de58e5e84e1729e
Abel Tran <abeltran.develop@gmail.com>
2022-12-10 15:35:34 +0700

    fix: grammar

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { AdminModule } from './core/admin/admin.module';
 import { CareModule } from './core/care/care.module';
 import { FriendModule } from './core/friend/friend.module';
 import { MemberModule } from './core/member/member.module';
-import { MinistryModule } from './core/ministry/ministnory.module';
+import { MinistryModule } from './core/ministry/ministry.module';
 import { TeamModule } from './core/team/team.module';
 
 @Module({
